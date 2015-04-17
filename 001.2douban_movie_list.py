1eea47f6dd915eb0a3b195961c8a244e330b3393
ZhangHang-z <2018464923@qq.com>
2015-04-17 08:11:24 +0800

    Update and rename 001.2douban_new_movie.py to 001.2douban_movie_list.py

--- 001.2douban_movie_list.py
+++ 001.2douban_movie_list.py
@@ -1,7 +1,7 @@
+#获取豆瓣新片榜， 口碑榜， 票房榜
 
 import re, urllib
-
-#获取豆瓣新片排行榜
+from bs4 import BeautifulSoup
 
 raw_url = 'http://movie.douban.com/chart'
 html = urllib.urlopen(raw_url).read()
@@ -49,3 +49,40 @@ def index(num):
 nn = len(a())
 for x in range(nn):
     index(x)
+
+
+# use bs4
+soup = BeautifulSoup(html)
+
+#本周口碑榜
+week = soup.find('div', id='ranking').find('ul', id='listCont2')
+print '-' * 80
+print '本周口碑榜....', soup.find('div', id='ranking').find('ul', id='listCont2') \
+      .find('li').get_text()
+for link in week.find_all('a'):
+    print link.get_text()
+
+
+#北美票房榜影名
+week = soup.find('div', id='ranking').find('ul', id='listCont1')
+usa = []
+for link in week.find_all('a'):
+    usa.append(link.get_text())
+
+#票房榜的钱
+money = soup.find('div', id='ranking').find('ul', id='listCont1')
+dollar = []
+for m in money.find_all('span'):
+    dollar.append(m.get_text())
+m_date = dollar.pop(0)
+
+def split(num2):
+    print usa[num2], dollar[num2]
+
+
+print '-' * 80
+print '北美票房榜....', m_date
+len_usa = len(usa)
+for fuck in range(len_usa):
+    split(fuck)
+
