c7639cec21924192e0a2356d9b05707ed26eec6b
ZhangHang-z <mail@alwme.com>
2015-05-04 09:36:20 +0800

    Create 006use_requests_login.py

---
+++ 006use_requests_login.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+''' The Python standard library urllib and urllib2 is really thoroughly broken, 
+requests is HTTP for human, used requests, you works will be very simple.
+The requests documentation  http://www.python-requests.org/en/latest/user/quickstart/  '''
+
+import requests
+import re
+from bs4 import BeautifulSoup
+import random
+import urllib, urllib2
+
+login_url = "http://www.douban.com/accounts/login"
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64)\
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36',}
+
+my_post = {'redir':'http://www.douban.com/doumail',
+    'form_email':'2018464923@qq.com',
+    'form_password':'7748548w',
+    'login':'登录',
+    }
+r = requests.post(login_url, data = my_post, headers = headers)
+html = r.text
+
+
+'''  download captcha  '''
+reg = r'<img id="captcha_image" src="(.*?)" alt="captcha" class="captcha_image"/>'
+imglist = re.findall(reg, html)
+urllib.urlretrieve(imglist[0], '%d.jpg' % random.randint(1,100))
+captcha = raw_input('captcha is: ')
+regid = r'<input type="hidden" name="captcha-id" value="(.*?)"/>'
+ids = re.findall(regid, html)
+
+''' repost  '''
+my_post["captcha-solution"] = captcha
+my_post["captcha-id"] = ids[0]
+q = requests.post(login_url, data = my_post, headers = headers)
+print q.url
+
+''' use bs4 filter douban.com's doumail below'''
+if q.url == "http://www.douban.com/doumail/":
+    print " login successfully!"
+    soup = BeautifulSoup(q.text)
+    tag = soup.find_all('span', attrs={'class':'from'})
+    tag2 =  soup.find_all('a', attrs={'class':'url'})
+    a = []
+    for x in tag:
+        a.append(x.get_text())
+    b = []
+    for y in tag2:
+        b.append(y.get_text())
+
+    def split(num):
+        print a[num] + '  ' +  b[num]
+        print 
+                    
+    print '-'*30, '豆瓣豆邮', '-'*30
+    for x in range(len(a)):
+        split(x)
+    print '-'*80
+
+    
+              
